4842e7ce34d2fe0609e31828d1d3d979d78bf2e2
jsyeh <jsyeh@mail.mcu.edu.tw>
2022-11-11 11:57:03 +0800

    修改目錄 week10

---
+++ week10/week10-1.cpp
@@ -0,0 +1,14 @@
+///Step1-1
+#include <stdio.h>
+int main()
+{
+    ///int a;
+    ///int a[4];///¦³4®æ
+    int a[4] = {10, 20, 30, 40};
+
+    printf("a[0]:%d\n", a[0] );
+    printf("a[1]:%d\n", a[1] );
+    printf("a[2]:%d\n", a[2] );
+    printf("a[3]:%d\n", a[3] );
+
+}
